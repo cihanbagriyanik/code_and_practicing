bf07e81c2efd86b4d97911730d1fe5c55853549c
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-07 13:47:26 +0200

    07.09.20233md

--- code/js/05-Loops/do-while/do-while.js
+++ code/js/05-Loops/do-while/do-while.js
@@ -1,13 +1,13 @@
 //!  ***** WHILE LOOP ******* 
-// For loop ile benzer şekilde sadece syntax farklılığı vardır.Birbirini yerine kullanılabilir
+//* For loop ile benzer şekilde sadece syntax farklılığı vardır.Birbirini yerine kullanılabilir
 //*"while" döngüsü, belirli bir koşul doğru olduğu sürece kod bloğunu tekrarlayan bir döngüdür. "While" döngüsü, başlangıçta belirtilen koşul doğru olduğu sürece çalışır ve koşul yanlış olduğunda döngüyü sonlandırır.
 //! NOT: Eğer koşul düzgün yapılındırılmamışsa, "while" döngüsü sonsuz bir döngü oluşturabilir ve bu durum sayfanızın veya uygulamanızın donmasına neden olabilir.
-// while(Koşul) {
-//      çalıştırılacak kodlar
-//   }
+//? while(Koşul) {
+//?      çalıştırılacak kodlar
+//?   }
 
 
-// 1den 10 a kadar olan sayıları yazdıran program
+//todo 1den 10 a kadar olan sayıları yazdıran program
 
 let i = 1;
 while (i <= 10) {
@@ -17,7 +17,7 @@ while (i <= 10) {
 
 
 
-//Kullanici 100 ve 200 arasinda bir deger girene kadar islemi devam ettiren program
+//todo Kullanici 100 ve 200 arasinda bir deger girene kadar islemi devam ettiren program
 
 // let number =90
 
@@ -27,7 +27,8 @@ while (i <= 10) {
 
 
 // console.log("100 ile 200 arasindabir deger girmediniz");
-/* -------------------------------------------------------------------------- */
+
+//! -------------------------------------------------------------------------- */
 
 // Cikis islemi
 // let cevap = "";
@@ -38,12 +39,12 @@ while (i <= 10) {
 
 console.clear();
 
-/* -------------------------------------------------------------------------- */
-// console.log(`Urun Listesi
-// 1-Urun giriniz
-// 2-Urun Listele
-// 3-Urun Sil
-// 4-Cikis`);
+//! -------------------------------------------------------------------------- */
+//? console.log(`Urun Listesi
+//? 1-Urun giriniz
+//? 2-Urun Listele
+//? 3-Urun Sil
+//? 4-Cikis`);
 
 /*
 let check = true;
@@ -77,12 +78,12 @@ while (check === true) {
 */
 
 
-//SAYI TAHMIN OYUNU
-// pc rastgele bir deger tutacak
-//Kullanici bulmaya calisacak.
-//Kullanicinin girdigi sayi bilgisayarin tuttugu sayidan kucuksa "daha buyuk sayi giriniz" desin
-//buyukse "daha kucuk bir sayi giriniz" desin
-//Bulursa tebrikler desin...
+//todo SAYI TAHMIN OYUNU
+//todo pc rastgele bir deger tutacak
+//todo Kullanici bulmaya calisacak.
+//todo Kullanicinin girdigi sayi bilgisayarin tuttugu sayidan kucuksa "daha buyuk sayi giriniz" desin
+//todo buyukse "daha kucuk bir sayi giriniz" desin
+//todo Bulursa tebrikler desin...
 
 
 
@@ -90,5 +91,5 @@ while (check === true) {
 
 
 
-// Girilen sayinin kac basamkli oldugunu bulan program WHILE LOOP ile yaziniz
-// 2346 sayisi 4 basamaklidir
+//todo Girilen sayinin kac basamkli oldugunu bulan program WHILE LOOP ile yaziniz
+//todo 2346 sayisi 4 basamaklidir
--- code/js/05-Loops/for-loop/for-loop.js
+++ code/js/05-Loops/for-loop/for-loop.js
@@ -1,21 +1,20 @@
 //! ********* FOR - LOOP ************
 // Karar durumları gibi döngülerde günlük hayatta çok karşımıza çıkan yapılardır.
 //Tekrarlı olan işlerde farklı türlerde döngülerden birini kullanabiliriz
-
 //? FOR -LOOP
 //? for (initialization,condition,increment/decrement){
-//      code block to be executed
+//?     code block to be executed
 //? }
 
 
-// 1 den 10 a kadar olan sayilari yazdiran blok
+//todo 1 den 10 a kadar olan sayilari yazdiran blok
 console.log("// 1 den 10 a kadar olan sayilari yazin");
 
 for (let i = 1; i <= 10; i++) {
     console.log(i);
 }
 
-// 10 den 1 a kadar olan sayilari yazdiran blok
+//todo 10 den 1 a kadar olan sayilari yazdiran blok
 console.log("// 1 den 10 a kadar olan sayilari yazin");
 
 for (let i = 10; i > 0; i--) {
@@ -23,7 +22,7 @@ for (let i = 10; i > 0; i--) {
 }
 
 
-// 1 den 100 e kadar olan sayilarin toplamini bulan yazdiran blok
+//todo 1 den 100 e kadar olan sayilarin toplamini bulan yazdiran blok
 console.log("// 1 den 100 e kadar olan sayilarin toplami yazin");
 
 let sum = 0;
@@ -32,8 +31,8 @@ for (let i = 1; i < 100; i++) {
 }
 console.log(`1 den 100 e kadar olan sayiarin toplami: ${sum}`);
 
-// 1 den 100 e kadar olan CIFT sayilarin toplamini bulan yazdiran blok
-// 1 Yol
+//todo 1 den 100 e kadar olan CIFT sayilarin toplamini bulan yazdiran blok
+//todo 1 Yol
 console.log("// 1 den 100 e kadar olan CIFT sayilarin toplami yazin");
 
 let result = 0;
@@ -42,7 +41,7 @@ for (i = 2; i < 100; i += 2) {
 }
 console.log(`toplam = ${result}`)
 
-// 2. Yol
+//todo 2. Yol
 let toplam = 0
 for (let i = 0; i < 100; i++) {
     i % 2 === 0 ? (toplam += i) : null
@@ -55,7 +54,7 @@ for (let i = 2; i < 100; i += 2) sum1 += i;
 console.log(sum1);
 
 
-// 1 den 100 e kadar olan TEK sayilarin toplamini bulan yazdiran blok
+//todo 1 den 100 e kadar olan TEK sayilarin toplamini bulan yazdiran blok
 console.log("// 1 den 100 e kadar olan TEK sayilarin toplami yazin");
 let res = 0;
 for (i = 1; i < 100; i += 2) {
@@ -65,11 +64,11 @@ console.log(`toplam = ${res}`)
 
 
 /* -------------------------------------------------------------------------- */
-//Kullanicidan alinan sayinin faktoriyelini bulan program
+//todoKullanicidan alinan sayinin faktoriyelini bulan program
 console.log("***** Kullanicidan alinan sayinin faktoriyelini bulan program *****");
 //1*2*3*4*5...=
 
-// let sayi = +prompt("sayi gir");
+//todo let sayi = +prompt("sayi gir");
 let sayi = 5
 let fakt = 1
 
@@ -79,7 +78,7 @@ for (i = 1; i <= sayi; i++) {
 console.log(`${sayi}'nin faktoriyeli = ${fakt}`)
 
 
-// ASAL SAYI algoritmasi
+//todo ASAL SAYI algoritmasi
 console.log("// 1 den 100 e kadar olan ASAL sayilari yazdiran blok");
 
 for (let i = 2; i < 100; i++) {
@@ -94,8 +93,8 @@ for (let i = 2; i < 100; i++) {
     }
 }
 
-/* -------------------------------------------------------------------------- */
-// Kullanicidan alinan sayinin asal olup olmadigini kontrol eden code
+//! -------------------------------------------------------------------------- */
+//todo Kullanicidan alinan sayinin asal olup olmadigini kontrol eden code
 
 // let num = +prompt("Bir sayi giriniz...")
 /*
@@ -120,11 +119,12 @@ if (num <= 1) {
 
 
 console.log("****************************************************************");
-/* -------------------------------------------------------------------------- */
-// Amstrong Number 
-// Bir sayının sayı değerlerin küplerinin toplamı kendine eşitse o sayı amstrong number olarak tanımlanır.
-// 153 = 1**3 + 5**3 + 3**3 
-//a- Kullanıcıdan girilen değer amstron number mı değil mi kontrol eden program
+//! -------------------------------------------------------------------------- */
+//todo Amstrong Number 
+//todo Bir sayının sayı değerlerin küplerinin toplamı kendine eşitse o sayı amstrong number olarak tanımlanır.
+//todo 153 = 1**3 + 5**3 + 3**3 
+//todo a- Kullanıcıdan girilen değer amstron number mı değil mi kontrol eden program
+
 let amsNumber=Number(prompt("Bir sayı giriniz:"))
 let birler=amsNumber %10
 let onlar =Math.trunc(amsNumber/10) %10
@@ -135,7 +135,7 @@ let result1=birler**3 + onlar**3+yuzler**3
 result1===amsNumber ? console.log(`${amsNumber} sayısı bir Amstrong Sayıdır`) : console.log(`${amsNumber} sayısı bir Amstrong Sayı değildir`)
 
 
-// 100den 1000e kadar olan sayılardaki amstrong numberları sıralayınız
+//todo 100den 1000e kadar olan sayılardaki amstrong numberları sıralayınız
 
 
 
