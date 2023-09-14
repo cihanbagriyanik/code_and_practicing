f81bdcbc66988cf956975149b47953879a75b442
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-14 09:50:32 +0200

    14.09.2023start

--- code/js/05-Loops/do-while/do-while.js
+++ code/js/05-Loops/do-while/do-while.js
@@ -1,125 +1,52 @@
-//!  ***** WHILE LOOP ******* 
-//* For loop ile benzer şekilde sadece syntax farklılığı vardır.Birbirini yerine kullanılabilir
-//*"while" döngüsü, belirli bir koşul doğru olduğu sürece kod bloğunu tekrarlayan bir döngüdür. "While" döngüsü, başlangıçta belirtilen koşul doğru olduğu sürece çalışır ve koşul yanlış olduğunda döngüyü sonlandırır.
-//! NOT: Eğer koşul düzgün yapılındırılmamışsa, "while" döngüsü sonsuz bir döngü oluşturabilir ve bu durum sayfanızın veya uygulamanızın donmasına neden olabilir.
-//? while(Koşul) {
-//?      çalıştırılacak kodlar
-//?   }
+/* -------------------------------------------------------------------------- */
+//!                                DO - WHILE                                 */
+/* -------------------------------------------------------------------------- */
+//* Kontrol sonda yapildigi icin, en az bir defa islem gerceklesir.
+//*  do {
+//*     calisacak kodlar
+//*  }
+//*  while(Kosul);
+
+// ? Kullanıcıdan vize ve final notlarını isteyen ve vizenin %40 ını finalin %60 ını alarak notunu hesaplayan her işlem bittiğinde devam etmek isteyip istemediğini soran program
+
+// let isContinue = "";
+// let Avg;
+
+// do {
+//   let vize = +prompt("Vize notunuzu Girin:");
+//   let final = +prompt("Final Notunu Giriniz:");
+
+//   Avg = vize * 0.4 + final * 0.6;
+//   console.log(`Ortalamanız : ${Avg}`);
+
+//   isContinue = prompt("Devam etmek istiyor musunuz(e/h):").toLowerCase();
+// } while (isContinue === "e");
 
 
-//! 1den 10 a kadar olan sayıları yazdıran program
+//? Girilen şifreye yada pin kodu doğru mu kontrolü
 
-let i = 1;
-while (i <= 10) {
-    console.log(i);
-    i++
-}
+// let setPassword = 1234;
+// let password;
+// let denemeSayisi = 0;
 
-
-
-//! Kullanici 100 ve 200 arasinda bir deger girene kadar islemi devam ettiren program
-
-// let number =90
-
-// while(number <= 100 || number>200){
-//     number = prompt("100 ile 200 arasinda bir deger giriniz.")
-// }
+// do{
+//     password = +prompt("Lutfen sifrenizi giriniz?")
+//     denemeSayisi++
 
 
-// console.log("100 ile 200 arasindabir deger girmediniz");
+// } while(password !== setPassword);
 
-//! -------------------------------------------------------------------------- */
-
-// Cikis islemi
-// let cevap = "";
-// while(cevap !== "evet"){
-//     cevap = prompt("Cikmak istiyor musunuz (evet / hayir):").toLowerCase()
-// }
-// console.log("Bizi tercih ettiginiz icin tesekkurler...");
-
-console.clear();
-
-//! -------------------------------------------------------------------------- */
-//? console.log(`Urun Listesi
-//? 1-Urun giriniz
-//? 2-Urun Listele
-//? 3-Urun Sil
-//? 4-Cikis`);
-
-/*
-let check = true;
-1
-while (check === true) {
-    console.log(`Urun Listesi
-               1-Urun giriniz
-               2-Urun Listele
-               3-Urun Sil
-               4-Cikis`);
-    let tercih = Number(prompt("Yapacaginiz islemin numarasini giriniz:"))
-
-    switch (tercih) {
-        case 1:
-            console.log("Urununuz girildi");
-            break;
-        case 2:
-            console.log("Urununuz listelendi");
-            break;
-        case 3:
-            console.log("Urununuz silindi");
-            break;
-        case 4:
-            check = false
-            break;
-        default:
-            console.log("Gecersiz giris yaptiniz.");
-            break;
-    }
-}
-*/
+// console.log(`${denemeSayisi} deneme sonrasinda basarili giris yaptiniz...`);
 
 
-//! -------------------------------------------------------------------------- */
+//? Kullanicidan bir sayi istiyoruz sayi degilse tekrar girsin (***isNaN()***)
+let check1 = isNaN("snfak"); // string oldugu icin true deger doner.
+console.log(check1);
+let check2 = isNaN(5); // sayi oldugu icin false deger doner.
+console.log(check2);
 
 
-//! SAYI TAHMIN OYUNU
-//? pc rastgele bir deger tutacak
-//? Kullanici bulmaya calisacak.
-//? Kullanicinin girdigi sayi bilgisayarin tuttugu sayidan kucuksa "daha buyuk sayi giriniz" desin
-//? buyukse "daha kucuk bir sayi giriniz" desin
-//? Bulursa tebrikler desin...
-
-
-let uretilenSayi = Math.round(Math.random() * 100);
-// console.log(uretilenSayi);
-let tahminEtmeSayisi = 0;
-let tahmin;
-
-while (true) {
-    let tahmin = +prompt("Please enter the number between 0 - 100...")
-    tahminEtmeSayisi++;
-
-    if (tahmin < uretilenSayi) {
-        alert("The estimated number is low! RAISE");
-    } else if (tahmin > uretilenSayi) {
-        alert("The estimated number is high! REDUCE");
-    } else {
-        console.log(`congratulations! You found the number in ${tahminEtmeSayisi} times!...`);
-        break;
-    }
-}
-
-
-//! -------------------------------------------------------------------------- */
-
-
-//! Girilen sayinin kac basamkli oldugunu bulan program WHILE LOOP ile yaziniz
-//? 2346 sayisi 4 basamaklidir
-
-let number = 2346;
-let digitCount = 0;
-
-while (number !== 0) {
-    digitCount++;
-    number = Math.trunc(number/10)
-}
-console.log(`Sayinin basamak sayisi: ${digitCount}`);
+// let inputNum;
+// do{
+//     inputNum = prompt("Bir sayi giriniz?")
+// } while (isNaN(inputNum)===true)
