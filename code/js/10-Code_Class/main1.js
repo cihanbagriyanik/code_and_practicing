2b0fcaf6e3e4ed1ba1cd95159856842d625ca7e8
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-19 13:13:54 +0200

    19.09.2023md

--- code/js/10-Code_Class/main1.js
+++ code/js/10-Code_Class/main1.js
@@ -29,3 +29,185 @@
 // trim()	            Removes whitespace from both ends of a string.
 // valueOf()	        Returns the primitive value of a String object.
 
+//* -------------------------------------------------------------------------- */
+
+const str1 = "Hello"
+const str2 = "World"
+console.log(str1 + str2);
+
+str1.concat(str2)
+console.log(str1.toLowerCase());
+console.log(str1.charAt(2));
+
+//* String icinde arama yapma
+console.log(str1.includes("al"));
+console.log(str1.indexOf("a"));
+console.log(str1.startsWith("H"));
+
+//* String Parcalama
+//* slice da eksi(-) deger kullanilabiliyor.
+//* substring() de eksi(-) deger kullanilmiyor
+console.log(str1.slice(1, 3));
+
+//* trim()
+const str = "   Merhaba     "
+console.log(str);
+console.log(str.trim());
+
+//* -------------------------------------------------------------------------- */
+
+//* split() - (Seperotor)
+console.log(str1.split(""));
+console.log(str1[0]);
+
+const cumle = "Merhaba D07 sinifi"
+console.log(cumle.split(" "));
+
+const cumle1 = "Merhaba/D07/sinifi"
+console.log(cumle1.split("/"));
+const splitedCumle = cumle1.split("/");
+console.log(splitedCumle);
+console.log(splitedCumle[0]);
+
+//* -------------------------------------------------------------------------- */
+
+//* split(),reverse(),join()
+//* reverse : Bir işlemi tersten yaptırır
+//* join: Liste elemanlarını birleştirir
+
+const tersten = str1.split("").reverse().join("")
+
+console.log(str1.split("").reverse().join(""));
+
+//* -------------------------------------------------------------------------- */
+console.clear();
+console.log("*****Code Class*****");
+
+/* -------------------------------------------------------------------------- */
+//*                                CODE CLASS                                 */
+/* -------------------------------------------------------------------------- */
+
+//? Soru-1 : Bir cumlenin kelime sayisini veren fonksiyon.
+
+const cumle2 = "Seni gidi findik kiran"
+const cumlesplit = cumle2.split(" ").length
+const kelimeSayisi = (cumlesplit) => {
+    return;
+}
+console.log(cumlesplit);
+
+//* -------------------------------------------------------------------------- */
+
+//? Soru-2 : Email giren kisiye kullanici adi olarak @ isaretinden once verdigi degeri veren 
+//todo  mert12@gmail.com => mert12
+
+const userName = (mail) => {
+
+    const mailWithoutAt = mail.replace("@", " ").split(" ")
+    console.log(mailWithoutAt[0]);
+
+}
+user = "mert21@gmail.com"
+userName(user)
+
+//* -------------------------------------------------------------------------- */
+
+//? Soru-3 : Bir cumleyi tersten yazan program 
+
+const reverse = (cumle) => {
+    const reverse = cumle.split("").reverse().join("")
+    console.log(reverse);
+}
+
+const cumle4 = "yakalarsam muah muah"
+reverse(cumle4)
+
+//? Girilen kelimenin polidrom olup olmadigini kontrol eden program
+
+const reverseWord = (word) => {
+    return word === word.split("").reverse().join("") ? "Polidromdur" : "Polidrom DEGILDIR"
+}
+
+console.log(reverseWord("level"));
+
+//? Girilen cumlenin polidrom olup olmadigini kontrol eden program
+
+const reversee = (sentence) => {
+
+    const orjinalSentence = sentence.toLowerCase().replaceAll(" ", "")
+    console.log(orjinalSentence);
+
+    return orjinalSentence === orjinalSentence.split("").reverse().join("") ? "Polidromdur" : "Polidrom DEGILDIR"
+}
+
+console.log(reversee("Ayol abla keten etek al baloya"));
+
+//* -------------------------------------------------------------------------- */
+
+//? Soru-4 : Belirli bir string ifadenin icindeki sesli harfleri bulan fonsiyonu yaziniz?
+//* 1.YOL :
+const sesliBul = (input) => {
+    const sesli = [];
+    let harf;
+
+    for (harf of input) {
+        if (['a', 'e', 'i', 'o', 'u'].includes(harf.toLowerCase())) {
+            sesli.push(harf);
+        }
+    }
+    return sesli;
+}
+console.log(sesliBul("Ayol abla keten etek al baloya"));
+
+//* 2.YOL :
+//todo - str.match()
+
+const sesliyiBul = (kelime) => {
+    const sesliHarfDeseni = /[aeiouAEOUI]/g;
+    const sesliHarfler = kelime.match(/[aeiouAEOUI]/g)
+    console.log(sesliHarfler);
+
+};
+
+sesliyiBul("Merhaba");
+
+//* -------------------------------------------------------------------------- */
+
+//? Soru-5 : ROMEN RAKAM dönüştürücü
+//todo  -  V: 57
+//todo  -  X :10
+//todo  -  I :1
+//todo  -  Kullanıcı bu şekilde girdiğinde   XXV - 25
+
+console.log();
+
+const cevir=(romen)=>{
+const parca=romen.split("")
+let sum=0;
+console.log(parca);
+for (let i=0; i<parca.length;i++){
+    switch (parca[i]) {
+        case "X":
+         sum +=10;   
+            break;
+        case "V":
+         sum +=5;   
+            break;
+        case "I":
+         sum +=1;   
+            break;
+
+        default:
+            break;
+    }
+}
+
+return sum
+
+}
+const romen="XVI"
+ console.log(cevir(romen));
+
+//*  "Run-Length Encoding" (RLE) sıkıştırma algoritması
+//*  "aaabbbccddddeee";
+//*  3a3b2c4d2e
