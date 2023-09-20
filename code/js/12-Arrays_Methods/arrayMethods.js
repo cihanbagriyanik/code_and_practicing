bb7205476aef06224f598302cbe7a2077ddddafb
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-20 12:24:40 +0200

    20.09.2023md

--- code/js/12-Arrays_Methods/arrayMethods.js
+++ code/js/12-Arrays_Methods/arrayMethods.js
@@ -7,7 +7,7 @@
 //*                     FOR LOOP
 //* ======================================================
 //*ÖRNEK: dizi1 arrayindeki pozitif sayılarla negatif sayıları 2 ayrı array de toplayan function yazınız
-
+console.log("*************** FOR LOOP **************");
 const dizi1 = [-5, 15, 22, -4, 45, 79, -54];
 
 const negatifler1 = []
@@ -30,7 +30,7 @@ dizilereAyir1()
 
 
 //*************** FOR IN **************/
-
+console.log("*************** FOR IN **************");
 const dizi2 = [-5, 15, 22, -4, 45, 79, -54]
 
 const negatifler2 = []
@@ -51,7 +51,7 @@ dizilereAyir2()
 
 
 //*************** FOR OF **************/
-
+console.log("*************** FOR OF **************");
 const dizi3 = [-5, 15, 22, -4, 45, 79, -54]
 
 const negatifler3 = []
@@ -69,3 +69,66 @@ const dizilereAyir3 = () => {
     console.log(pozitifler3);
 }
 dizilereAyir3()
+
+
+
+//* ======================================================
+//*                   FOR IN LOOP
+//* ======================================================
+
+//*-------------------------------------------------------
+//? SORU: Bir hayvanat bahçesinde bulunan hayvan türlerinin
+//? bir dizide saklandığını varsayalım.Bu hayvanları türüne
+//? göre aramamızı sağlayacak ve o türden kaç adet bulunduğunu
+//? ana programa döndürecek fonksiyonu yazınız.
+//*--------------------------------------------------------
+console.log("*************** FOR IN LOOP SORU **************");
+/*
+const animal = [
+    "fil",
+    "aslan",
+    "deve",
+    "fil",
+    "kaplan",
+    "fil",
+    "deve",
+    "aslan",
+    "aslan",
+    "aslan",
+];
+
+const userAnimal = prompt("Lutfen bir hayvan adi giriniz")
+
+const findAnimal = () => {
+    let sayac = 0 ;
+    for (let i in animal) {
+        if(animal[i] == userAnimal) {
+            sayac++;
+        }
+    }
+    // console.log(`${userAnimal} hayfanindan ${sayac} tane bulunmaktadir`);
+    return `${userAnimal} hayfanindan ${sayac} tane bulunmaktadir`
+}
+console.log(findAnimal());
+*/
+
+
+
+//* ======================================================
+//*                   FOR OF LOOP
+//* ======================================================
+//* for of dongusu, for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir. Bu dongude dizi icindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+//? SORU: Alttaki dizinin elemanlarını yanyana string olarak yazdır (for of döngüsü kullanın)
+console.log("*************** FOR OF LOOP SORU **************");
+
+const arabalar = ["bmw", "mercedes", "audi", "volvo"]
+
+//! 1. YOL
+console.log(arabalar.join(""));
+
+//! 2.YOL
+let araba = ""
+for (let i of arabalar) {
+    araba += i // araba = araba + i
+}
+console.log(araba);
