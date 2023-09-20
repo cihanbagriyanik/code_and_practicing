2cc06870b445bd180e87c0d41e8533d6ea6f14d9
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-20 19:30:50 +0200

    20.09.2023 JsSelfPracticing Conditionals

--- self_practicing/js/01-conditionals(if,else-switchCase-ternary-shortCircuit)/main.js
+++ self_practicing/js/01-conditionals(if,else-switchCase-ternary-shortCircuit)/main.js
@@ -3,9 +3,9 @@
 
 // const num = +prompt("Lutfen bir sayi giriniz...")
 const num = 0
-if (num>0) {
+if (num > 0) {
     console.log("Girdiginiz sayi POZITIF bir sayidir...");
-} else if (num<0) {
+} else if (num < 0) {
     console.log("Girdiginiz sayi NEGATIF bir sayidir...");
 } else {
     console.log("Girdiginiz sayi NOTR(0)'dir...");
@@ -13,17 +13,50 @@ if (num>0) {
 
 //? Consale dan  3tam sayi alarak bunlarin en buyunu yazdirin
 
-const nr1 = +prompt("Lutfen karsilastirmak istediginiz 3 numaradan 1. sini giriniz...")
-const nr2 = +prompt("Lutfen karsilastirmak istediginiz 3 numaradan 2. sini giriniz...")
-const nr3 = +prompt("Lutfen karsilastirmak istediginiz 3 numaradan 3. sini giriniz...")
+// const nr1 = +prompt("Lutfen karsilastirmak istediginiz 3 numaradan 1. sini giriniz...")
+// const nr2 = +prompt("Lutfen karsilastirmak istediginiz 3 numaradan 2. sini giriniz...")
+// const nr3 = +prompt("Lutfen karsilastirmak istediginiz 3 numaradan 3. sini giriniz...")
+const nr1 = 5
+const nr2 = 5
+const nr3 = 5
 
-
-if (nr1>nr2 && nr1>nr3) {
-    console.log(`Girinlen sayilar arasinda ${nr1} en buyugudur...`);
-} else if (nr2>nr1 && nr2>nr3) {
-    console.log(`Girinlen sayilar arasinda ${nr2} en buyugudur...`);
-} else if (nr3>nr1 && nr3>nr2) {
-    console.log(`Girinlen sayilar arasinda ${nr3} en buyugudur...`);
+if (nr1 > nr2 && nr1 > nr3) {
+    console.log(`Girinlen 1. sayi sayilar arasinda en buyugudur: ${nr1}`);
+} else if (nr2 > nr1 && nr2 > nr3) {
+    console.log(`Girinlen 2. sayi sayilar arasinda en buyugudur: ${nr2}`);
+} else if (nr3 > nr1 && nr3 > nr2) {
+    console.log(`Girinlen 3. sayi sayilar arasinda en buyugudur: ${nr3}`);
 } else {
     console.log(`Girinlen sayilar birbirine esittir...`);
+}
+
+//? 4 Islem makinesi yaziniz (2 Adet sayi icin)
+
+const islem = prompt("Yapmak istediğiniz işlemi seçiniz (+, -, *, /):");
+
+if (islem === "+" || islem === "-" || islem === "*" || islem === "/") {
+    const sayi1 = +prompt("Birinci sayıyı giriniz:");
+    const sayi2 = +prompt("İkinci sayıyı giriniz:");
+
+    let sonuc;
+
+    if (islem === "+") {
+        sonuc = sayi1 + sayi2;
+        console.log(`Sonuç: ${sayi1} + ${sayi2} = ${sonuc}`);
+    } else if (islem === "-") {
+        sonuc = sayi1 - sayi2;
+        console.log(`Sonuç: ${sayi1} - ${sayi2} = ${sonuc}`);
+    } else if (islem === "*") {
+        sonuc = sayi1 * sayi2;
+        console.log(`Sonuç: ${sayi1} * ${sayi2} = ${sonuc}`);
+    } else if (islem === "/") {
+        if (sayi2 !== 0) {
+            sonuc = sayi1 / sayi2;
+            console.log(`Sonuç: ${sayi1} / ${sayi2} = ${sonuc}`);
+        } else {
+            console.log("Bölme işlemi sıfıra bölünemez.");
+        }
+    }
+} else {
+    console.log("Geçersiz işlem seçtiniz. Lütfen +, -, *, / işlemlerinden birini seçiniz.");
 }
