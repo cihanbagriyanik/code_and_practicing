233eb1f8959545bf5313de02d0edf911345cd147
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-20 18:08:41 +0200

    20.09.2023 JsSelfPracticing Conditionals

--- self_practicing/js/01-conditionals(if,else-switchCase-ternary-shortCircuit)/main.js
+++ self_practicing/js/01-conditionals(if,else-switchCase-ternary-shortCircuit)/main.js
@@ -1,12 +1,29 @@
 
 //? Console dan girilen bir sayinin pozitif, negatif yada 0 oldugunu tespit ederk yaziniz?
 
-const num = +prompt("Lutfen bir sayi giriniz...")
-
+// const num = +prompt("Lutfen bir sayi giriniz...")
+const num = 0
 if (num>0) {
     console.log("Girdiginiz sayi POZITIF bir sayidir...");
 } else if (num<0) {
     console.log("Girdiginiz sayi NEGATIF bir sayidir...");
 } else {
     console.log("Girdiginiz sayi NOTR(0)'dir...");
+}
+
+//? Consale dan  3tam sayi alarak bunlarin en buyunu yazdirin
+
+const nr1 = +prompt("Lutfen karsilastirmak istediginiz 3 numaradan 1. sini giriniz...")
+const nr2 = +prompt("Lutfen karsilastirmak istediginiz 3 numaradan 2. sini giriniz...")
+const nr3 = +prompt("Lutfen karsilastirmak istediginiz 3 numaradan 3. sini giriniz...")
+
+
+if (nr1>nr2 && nr1>nr3) {
+    console.log(`Girinlen sayilar arasinda ${nr1} en buyugudur...`);
+} else if (nr2>nr1 && nr2>nr3) {
+    console.log(`Girinlen sayilar arasinda ${nr2} en buyugudur...`);
+} else if (nr3>nr1 && nr3>nr2) {
+    console.log(`Girinlen sayilar arasinda ${nr3} en buyugudur...`);
+} else {
+    console.log(`Girinlen sayilar birbirine esittir...`);
 }
