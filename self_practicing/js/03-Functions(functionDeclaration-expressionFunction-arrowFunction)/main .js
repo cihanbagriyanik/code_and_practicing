4268b5f6ebe3eee01079d76b5eb38e543dfd2b88
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-22 09:06:45 +0200

    22.09.2023 functionSelf Start

--- self_practicing/js/03-Functions(functionDeclaration-expressionFunction-arrowFunction)/main .js
+++ self_practicing/js/03-Functions(functionDeclaration-expressionFunction-arrowFunction)/main .js
@@ -1,23 +1,30 @@
 //? 4 işlem hesap makinası nı fonksiyonlari ile tekrardan yazalım.
 
 function toplama(a, b) {
-   return a + b
+    return a + b
 }
 function cikarma(a, b) {
-   return a - b
+    return a - b
 }
 function bolme(a, b) {
-   return a / b
+    return a / b
 }
 function carpma(a, b) {
-   return a * b
+    return a * b
 }
-console.log(toplama(5,55));
-console.log(cikarma(55,5));
-console.log(carpma(5,5));
-console.log(bolme(50,5));
+console.log(toplama(5, 55));
+console.log(cikarma(55, 5));
+console.log(carpma(5, 5));
+console.log(bolme(50, 5));
 
 
 //? Bir sayıyı parametre olarak alan ve bu sayının tek veya çift olduğunu hesaplayıp sonucu ana programa döndüren fonksiyonu yazınız.
 //todo •Fonksiyon sonucu TEK veya ÇİFT olarak döndürmelidir ve sonuç ana programda ekrana bastırılmalıdır.
 
+function tekMiciftMi(a) {
+    return (a % 2 === 0) ? "CIFT" : "TEK"
+}
+console.log(tekMiciftMi(2));
+
+//* -------------------------------------------------------------------------- */
+//! ARROW FUNCTION
