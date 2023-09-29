ae1205c62bd7a5bcc22bc280d60758ff593ee91c
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-29 19:35:15 +0200

    29.03.2023 DomPractice Mid

--- code/js/18.1-DOM_GuessNumber/main.js
+++ code/js/18.1-DOM_GuessNumber/main.js
@@ -1,23 +1,42 @@
-// rastgele sayı tutma işleminde 1 ile 20 arasında bir sayıya ihtiyacımız varsa Math.random() ı 20 ile çarpıp yukarı yuvarlamalıyız
-let PCsayi=  Math.ceil(Math.random()*20);
 
-let mesaj=document.querySelector(".msg")
+// rastgele sayı tutma işleminde 1 ile 20 arasında bir sayıya ihtiyacımız varsa Math.random() ı 20 ile çarpıp yukarı yuvarlamalıyız
+let PCsayi = Math.ceil(Math.random() * 20);
+
+let mesaj = document.querySelector(".msg");
+
+//? Enter tuşuna basılınca "check" butonuna tıklanmasını sağliyorum
+document.querySelector(".guess").onkeydown = (tus) => {
+    if (tus.keyCode === 13) {
+        document.querySelector(".check").click();
+    }
+};
 
 //? her check butonuna basılınca yapılacaklar
+document.querySelector(".check").onclick = () => {
+    // Farkli bir renk dizisi tanimliyoruz
+    const renkler = ["red", "blue", "pink", "purple", "brown", "DarkAqua", "DarkOrange", "Fuchsia"];
+    // Tanimladigmiz dizilerin rastgele gelmesini sagliyoruz
+    const rasgeleRenk = Math.floor(Math.random() * renkler.length);
+    
+    const tahmin = +document.querySelector(".guess").value;
 
-
-document.querySelector(".check").onclick=()=>{
-const tahmin=document.querySelector(".guess").value
-
-
-//*tahmin doğruysa
-
-//*tahmin yanlışsa
-//* bizi arttır azalt diye yönlendir
-
-
-
-
-
-
-}
+    document.querySelector(".guess").value = "";
+    // tahmin doğruysa
+    if (PCsayi === tahmin) {
+        mesaj.innerHTML = "Tebrikler, Bildiniz";
+        document.querySelector(".body").style.backgroundColor = "green";
+        document.querySelector(".number").innerHTML = PCsayi;
+    }
+    // tahmin yanlışsa
+    // bizi arttır azalt diye yönlendir
+    else if (PCsayi > tahmin) {
+        mesaj.innerHTML = "Tahmin düşük, yükseltin";
+        document.querySelector(".body").style.backgroundColor = renkler[rasgeleRenk];
+    }
+    else if (PCsayi < tahmin) {
+        mesaj.innerHTML = "Tahmin yüksek, düşürün";
+        document.querySelector(".body").style.backgroundColor = renkler[rasgeleRenk];
+    } else {
+        mesaj.innerHTML = ""; // Hata durumunda mesajı temizliyorum
+    }
+};
