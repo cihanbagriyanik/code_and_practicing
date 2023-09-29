7235451477a77b416bd445a363dc34bf3de53447
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-29 21:46:36 +0200

    ClassFolder Removing

--- class/linux/practicing/Clarusway_LLC/Full_Stack/Backend/MySQL/text.txt
+++
@@ -1,5 +0,0 @@
-SELECT *
-FROM employees
-WHERE salary > 10000
-ORDER BY salary DESC
-LIMIT 3
--- class/linux/practicing/Ders2/permission/ex.sh
+++
@@ -1,7 +0,0 @@
-echo "###### Im an executable file ######"
-sleep 2
-echo "###### I will created three different folder ######"
-sleep 3
-mkdir folder1 folder2 folder3
-echo "command was executed"
-sleep3
