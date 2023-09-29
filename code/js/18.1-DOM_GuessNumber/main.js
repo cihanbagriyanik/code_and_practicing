776b3aff7d620673ced28454d3b68e9c40c858e6
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-09-29 13:02:47 +0200

    test guessNumber Problem

---
+++ code/js/18.1-DOM_GuessNumber/main.js
@@ -0,0 +1,23 @@
+// rastgele sayı tutma işleminde 1 ile 20 arasında bir sayıya ihtiyacımız varsa Math.random() ı 20 ile çarpıp yukarı yuvarlamalıyız
+let PCsayi=  Math.ceil(Math.random()*20);
+
+let mesaj=document.querySelector(".msg")
+
+//? her check butonuna basılınca yapılacaklar
+
+
+document.querySelector(".check").onclick=()=>{
+const tahmin=document.querySelector(".guess").value
+
+
+//*tahmin doğruysa
+
+//*tahmin yanlışsa
+//* bizi arttır azalt diye yönlendir
+
+
+
+
+
+
+}
