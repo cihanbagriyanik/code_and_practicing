3e66b0dfe8e25759f5184c8bd665ebb3af2e78f4
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-05 11:33:21 +0200

    05.10.2023 todoList md

---
+++ code/js/22-ToDoApp Project/self/main.js
@@ -0,0 +1,13 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("First you must write something!")
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li)
+    }
+    inputBox.value = ""
+}
