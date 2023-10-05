b3da49a45e5d26a63108884991ad69fed23c4908
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-05 12:40:17 +0200

    05.10.2023 todoList md

--- code/js/22-ToDoApp Project/self/main.js
+++ code/js/22-ToDoApp Project/self/main.js
@@ -1,7 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-let x = []
+// let x = []
 // localStorage.clear();
 
 function addTask() {
@@ -15,23 +15,23 @@ function addTask() {
         span.innerHTML = "\u00d7"
         li.appendChild(span)
     }
-    x.push(inputBox.value)
+    // x.push(inputBox.value)
     inputBox.value = ""
-    saveData(x);
+    saveData();
 }
 
 listContainer.addEventListener("click", function(e){
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked")
-        saveData(x)
+        saveData()
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        saveData(x)
+        saveData()
     }
 }, false)
 
-function  saveData(x) {
-    localStorage.setItem("data", x);
+function  saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
 }
 
 function showTask() {
