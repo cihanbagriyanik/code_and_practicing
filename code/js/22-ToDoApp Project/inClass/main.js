f47020039a631eb0935d6b1bec344530ea55721a
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-06 11:45:15 +0200

    06.10.2023 md

--- code/js/22-ToDoApp Project/inClass/main.js
+++ code/js/22-ToDoApp Project/inClass/main.js
@@ -1,72 +1,89 @@
 
-let liste = []
 //! boş liste hem database işini kolaylaştırır, hem ul ye eklenen verinin aynısı tekrar eklenmesin demek için ,bütün p leri çağır içeriğinde input.value deki değer varsa ekleme, demeli ama p oluşmadan bunu diyemeyiz, bu şekilde liste dizisine eklenen veri globalde old için içeriğine (includes) her yerden bakılabilir
+let liste = [];
 
-
-let total = 0
-let completed = 0
+let total = 0;
+let completed = 0;
 
 const listeInput = document.querySelector("#todo-input");
 const listeUl = document.querySelector("#todo-ul");
 const listeButon = document.querySelector("#todo-button");
 const toplam = document.querySelector("#toplam");
 
-//! Add butonuna tiklandiginda
+//! Add butonuna tıklandığında
 listeButon.onclick = () => {
-
     if (!listeInput.value) {
-        alert("Lutfen bir not giriniz")
-        return;
+        alert("lütfen bir not giriniz");
     }
+
+    //! input a girilen veri (Add e basınca) daha önce listeye girildiyse, tekrar girilmesin, boş dön, bu kontrolü mesela ekrandaki li leri çekerek yapamayız, çünkü includes  lar dizi de çalışır
     else if (liste.includes(listeInput.value)) {
-        alert("Daha once bu notu girmistiniz")
-        return; // seadece returnle de olabilir ama kullaniciya bir sey dondurmemis olur.
+        return;
+        // return=if in içindeki şart true ise hiçbirşey yapma, koda alt satırdan devam et demek
+    } else {
+        liste.push(listeInput.value);
+        total += 1;
+
+        //!listeyi ekranda gör
+
+        listeUl.innerHTML += `
+            <li class="esra">
+                <i class="fa fa-check fa-lg"></i>
+
+                    <p>${liste[liste.length - 1]}</p>
+
+                <i class="fa fa-trash fa-lg"></i>
+            </li>
+        `;
+        //?2.seçenek
+        //  <p>${listeInput.value}</p>;
+
+
+        //!total i ekrana bastır
+        toplam.textContent = total;
+
+        listeInput.value = "";
+
+        //? her li girişinde cursor inputta olsun
+        listeInput.focus();
     }
-    else {
-        liste.push(listeInput.value)
-        total += 1
-    }
 
-    //! listeyi ekranda gor
-    listeUl.innerHTML += `
-    <li class="esra">
-    <i class="fa fa-check fa-lg"></i>
-    
-    <p> ${liste[liste.length - 1]}</p>
- 
-    <i class="fa fa-trash fa-lg"></i>
-    </li>
-    `;
-    //? 2. secenek <p> tagi ekleme
-    // {/* <p>${listeInput.value}</p> */}
-
-
-    //! Total i ekrana bastir
-    toplam.textContent = total
-
-    //! Deger girildikten sonra input u bize bos donduruyor.
-    listeInput.value = ""
-
-    //! Her li girisinde cursor inputta olsun
-    listeInput.focus()
-
-
-    //! Silme islemi
-    //! Bu islemde icerde yapiliyor cunku htmlde bir cop kutusu iconu yok add a basilinca geliyor.
-    // createSilButon()
-    document.querySelectorAll(".fa-trash").forEach((a) => a.onclick = () => {
-        //? Ekrandan sil
-        a.parentElement.remove()
-    })
-
-    //? Diziden sil
+    //!silme işlemi
+    createSilButon()
 
 
 
+    //!check işlemi
+    createCheckButon()
 
+};
 
+const createSilButon = () => {
 
-    //! Check islemi
+    document.querySelectorAll(".fa-trash").forEach(
+        (a) =>
+        (a.onclick = () => {
+            //?ekrandan sil
+
+            //1.yol
+            a.parentElement.remove();
+            //2.yol
+            // a.closest("li").remove()
+            total -= 1;
+            toplam.textContent = total;
+
+            //?diziden sil
+
+            liste = liste.filter(
+                (yapilacak) =>
+                    yapilacak != a.parentElement.querySelector("p").textContent
+            );
+        })
+    );
+}
+
+const createCheckButon = () => {
+
 
 }
 
