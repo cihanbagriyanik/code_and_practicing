5ae8a6e4cc9cfcb25827b037de878bf15e0e5cf6
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-16 21:20:42 +0400

    16.10.2023 checkout end

--- code/js/23-Checkout Project/checkout.js
+++ code/js/23-Checkout Project/checkout.js
@@ -2,7 +2,7 @@
 //*                 Checkout Page Solution
 //*  map filter, dest,spread ===================================================
 //!table da kullanılacak değişkenler
-const kargo = 15.0;
+const kargo = 15.00;
 const vergi = 0.18;
 
 let sepettekiler = [
@@ -31,12 +31,12 @@ sepettekiler.forEach((urun) => {
 
         <div class="card-body">
 
-          <h5 class="card-title">${name} </h5>
+          <h5 class="card-title">${name}</h5>
 
           <div class="ürün-price">
             <p class="text-warning h2">$
-              <span class="indirim-price">${(price * 0.7).toFixed(2)} </span>
-              <span class="h5 text-dark text-decoration-line-through">${price} </span>
+              <span class="indirim-price">${(price * 0.7).toFixed(2)}</span>
+              <span class="h5 text-dark text-decoration-line-through">${(price).toFixed(2)}</span>
             </p>
           </div>
 
@@ -48,7 +48,7 @@ sepettekiler.forEach((urun) => {
               <button class="btn btn-secondary btn-sm minus">
                 <i class="fas fa-minus"></i>
               </button>
-              <p class="d-inline mx-4" id="ürün-adet">${adet} </p>
+              <p class="d-inline mx-4" id="ürün-adet">${adet}</p>
               <button class="btn btn-secondary btn-sm plus">
                 <i class="fas fa-plus"></i>
               </button>
@@ -63,7 +63,7 @@ sepettekiler.forEach((urun) => {
           </div>
 
           <div class="mt-2">
-            Ürün Toplam: $<span class="ürün-toplam">${(price * 0.7).toFixed(2) * adet} </span>
+            Ürün Toplam: $<span class="ürün-toplam">${(price * 0.7).toFixed(2) * adet}</span>
           </div>
         </div>
       </div>
@@ -85,43 +85,101 @@ sil();
 function sil() {
 
   document.querySelectorAll(".remove-ürün").forEach((btn) => {
-  
+
     btn.onclick = () => {
-      
+      alert("Emin misiniz?")
+      //! Ekrandan sildik
       btn.closest(".card").remove()
+
+
+      //! Diziden de sil
+      sepettekiler = sepettekiler.filter((urun) => urun.name != btn.closest(".card").querySelector("h5").textContent)
+
+      // console.log(sepettekiler);
+
 
       //! Hesapla Total
       hesaplaCardTotal()
 
+    };
+  });
+};
+
+//! ADET DEGISTIRME
+document.querySelectorAll(".adet-controller").forEach((kutu) => {
+
+  const arti = kutu.lastElementChild
+  const eksi = kutu.firstElementChild
+  const adet1 = kutu.children[1]
+  // const adet1 = arti.previousElementSibling
+  // const adet1 = eksi.nextElementSibling
+
+  //! artiya tiklaninca olacaklar
+  arti.onclick = () => {
+
+    //* ekranda degistirme
+    adet1.textContent = +adet1.textContent + 1
+
+    //* Urun Toplam kismini guncelle 
+    adet1.closest(".card").querySelector(".ürün-toplam").textContent = adet1.textContent * adet1.closest(".card").querySelector(".indirim-price").textContent
+
+    //? dizideki adeti guncelle
+    sepettekiler = sepettekiler.map((a) => {
+      if (a.name == arti.closest(".card").querySelector("h5").textContent) {
+        return { ...a, adet: +adet1.textContent }
+      } else { return a }
+    })
+    console.log(sepettekiler);
+
+
+
+    hesaplaCardTotal();
+
+  }
+
+
+
+
+  //! eksiye tiklaninca olacaklar
+  eksi.onclick = () => {
+
+    //* ekranda degistirme
+    adet1.textContent = adet1.textContent - 1
+
+    //* Urun Toplam kismini guncelle 
+    adet1.closest(".card").querySelector(".ürün-toplam").textContent = adet1.textContent * adet1.closest(".card").querySelector(".indirim-price").textContent
+
+    //* eger adet 1 iken tekrar eksiye basilirsa o urunu sil
+    if (adet1.textContent < 1) {
+      alert("Sileyim mi?")
+      adet1.closest(".card").remove()
     }
 
+    //? dizideki adeti guncelle
+    sepettekiler = sepettekiler.map((a) => {
+      if (a.name == eksi.closest(".card").querySelector("h5").textContent) {
+        return { ...a, adet: +adet1.textContent }
+      } else { return a }
+    })
+    console.log(sepettekiler);
 
 
-  })
+    hesaplaCardTotal();
 
-
-}
-
-
-
-
-
-
-
-
-
+  }
 
 
 
+})
 
 
 //! BROWSERDAKI TOTAL I HESAPLAMA
 function hesaplaCardTotal() {
-  
+
   const urunToplam = document.querySelectorAll(".ürün-toplam");
   // console.log(urunToplam); // NodeList cikti
-  
-  
+
+
   //!   querySelectorAll(), statik bir NodeList döndürür.
   //!burada netten https://softauthor.com/javascript-htmlcollection-vs-nodelist/ adresinden göster
   // Dizi Değil!
@@ -130,14 +188,14 @@ function hesaplaCardTotal() {
   // Ancak, bir NodeList'te push(), pop() veya join() gibi Array yöntemlerini kullanamazsınız.
   //* Reduce tam olarak Array istiyor, nodeList yeterli degil
   // console.log(Array.from(urunToplam)); // Array cikti
-  
-  
+
+
   const araToplam = Array.from(urunToplam).reduce((topl, item) => topl + +item.textContent, 0);
-  
-  document.querySelector(".aratoplam").textContent = araToplam
+
+  document.querySelector(".aratoplam").textContent = (araToplam).toFixed(2)
   document.querySelector(".vergi").textContent = (araToplam * vergi).toFixed(2)
-  document.querySelector(".kargo").textContent = kargo
-  document.querySelector(".toplam").textContent = (araToplam + (araToplam * vergi) + kargo).toFixed(2)
+  document.querySelector(".kargo").textContent = (araToplam > 0 ? kargo : 0).toFixed(2);
+  document.querySelector(".toplam").textContent = araToplam > 0 ? (araToplam + (araToplam * vergi) + kargo).toFixed(2) : (0).toFixed(2)
 
 }
 
