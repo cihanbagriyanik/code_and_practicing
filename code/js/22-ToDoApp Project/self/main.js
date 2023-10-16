449b6e5a3c31a54d146fa87d4864fd987773ab2f
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-16 11:13:57 +0400

    16.10.2023 start

--- code/js/22-ToDoApp Project/self/main.js
+++ code/js/22-ToDoApp Project/self/main.js
@@ -41,98 +41,224 @@
 
 //* -------------------------------------------------------------------------- */
 
+// //? Here is the list for storge
+// const list = JSON.parse(localStorage.getItem("LIST")) || [];
+
+// //? Variables
+// const input = document.getElementById("input-box")
+// const button = document.querySelector(".button")
+// const listContainer = document.getElementById("list-container")
+
+
+// //? When you push the button
+// button.onclick = () => {
+//     if (!input.value) {
+//         alert("Please firstly enter one task")
+//     } else if (list.includes(input.value)) {
+//         alert("You have already this task")
+//     } else {
+//         list.push(input.value)
+//         // console.log(list);
+
+//         //? When you enter the task to the list, update the list 
+//         localStorage.setItem("LIST", JSON.stringify(list))
+
+
+//         // console.log(list);
+        
+//         input.focus();
+    
+//         //? After enter the value return empty input
+//         input.value = ""
+
+//         showList();
+
+//     }
+
+// }
+
+// const showList = () => {
+    
+//     //? Clear the last input in data
+//     listContainer.innerHTML = "";
+    
+//     for (let i = 0; i < list.length; i++) {
+//         const li = document.createElement("li");
+//         li.textContent = list[i];
+        
+//         const span = document.createElement("span");
+//         span.textContent = "\u00d7";
+        
+//         li.appendChild(span);
+//         listContainer.appendChild(li);
+        
+//     }
+    
+//     remove();
+//     // check();
+// }
+
+
+// const remove = () => {
+
+//     document.querySelectorAll("span").forEach((span) => {
+//         span.addEventListener("click", () => {
+            
+//              //? change to 'span'
+//             span.parentElement.remove();
+
+//             let taskText = span.parentElement.textContent;
+//             let taskIndex = list.indexOf(taskText);
+//             console.log(span.parentElement.textContent);
+            
+//             //? delete the task from list
+//            list.splice(taskIndex,1)
+        
+            
+//             //? save the updated version of list
+//             localStorage.setItem("LIST", JSON.stringify(list)); 
+//             showList(); // Görevleri güncel liste ile tekrar göster
+            
+//         });
+//     });
+// };
+
+
+// listContainer.addEventListener("click", function(e){
+//     if (e.target.tagName === "LI") {
+//         e.target.classList.toggle("checked")
+        
+//     } else if (e.target.tagName === "SPAN") {
+//         e.target.parentElement.remove();
+        
+//     }
+// }, false)
+
+
+// showList();
+
+
+/* -------------------------------------------------------------------------- */
+
+
 //? Here is the list for storge
 const list = JSON.parse(localStorage.getItem("LIST")) || [];
 
 //? Variables
-const input = document.getElementById("input-box")
-const button = document.querySelector(".button")
-const listContainer = document.getElementById("list-container")
-
+const input = document.getElementById("input-box");
+const button = document.querySelector(".button");
+const listContainer = document.getElementById("list-container");
 
 //? When you push the button
 button.onclick = () => {
-    if (!input.value) {
-        alert("Please firstly enter one task")
-    } else if (list.includes(input.value)) {
-        alert("You have already this task")
-    } else {
-        list.push(input.value)
-        // console.log(list);
+  if (!input.value) {
+    alert("Please firstly enter one task");
+  } else if (list.includes(input.value)) {
+    alert("You have already this task");
+  } else {
+    list.push(input.value);
+    // console.log(list);
 
-        //? When you enter the task to the list, update the list 
-        localStorage.setItem("LIST", JSON.stringify(list))
+    //? When you enter the task to the list, update the list
+    localStorage.setItem("LIST", JSON.stringify(list));
 
+    // console.log(list);
 
-        // console.log(list);
-        
-        input.focus();
-    
-        //? After enter the value return empty input
-        input.value = ""
+    input.focus();
 
-        showList();
+    //? After enter the value return empty input
+    input.value = "";
 
-    }
+    showList();
+  }
+};
 
-}
+// const showList = () => {
+//   //? Clear the last input in data
+//   listContainer.innerHTML = "";
+
+//   for (let i = 0; i < list.length; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = list[i];
+
+//     const span = document.createElement("span");
+//     span.textContent = "\u00d7";
+
+//     li.appendChild(span);
+   
+//     listContainer.appendChild(li);
+//   }
+
+//   remove();
+//   // check();
+// };
+
+// const getSymbolForItem = (index) => {
+//     // Özel bir mantıkla her öğe için farklı bir simge döndürün
+//     // Burada farklı durumlara göre farklı simgeler döndürebilirsiniz
+//     return "\u00d7"; // Örneğin, 'x' işareti
+// };
 
 const showList = () => {
-    
-    //? Clear the last input in data
-    listContainer.innerHTML = "";
-    
-    for (let i = 0; i < list.length; i++) {
-        const li = document.createElement("li");
-        li.textContent = list[i];
-        
-        const span = document.createElement("span");
-        span.textContent = "\u00d7";
-        
-        li.appendChild(span);
-        listContainer.appendChild(li);
-        
-    }
-    
-    remove();
-    // check();
-}
+  listContainer.innerHTML = "";
 
+  for (let i = 0; i < list.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = list[i];
 
-const remove = () => {
+    const span = document.createElement("span");
+    const symbol = getSymbolForItem(i); // Her öğe için farklı bir simge alın
 
-    document.querySelectorAll("span").forEach((span) => {
-        span.addEventListener("click", () => {
-            
-             //? change to 'span'
-            span.parentElement.remove();
+    span.textContent = symbol; // Sembolü ekleyin
 
-            let taskText = span.parentElement.textContent;
-            let taskIndex = list.indexOf(taskText);
-            console.log(span.parentElement.textContent);
-            
-            //? delete the task from list
-           list.splice(taskIndex,1)
-        
-            
-            //? save the updated version of list
-            localStorage.setItem("LIST", JSON.stringify(list)); 
-            showList(); // Görevleri güncel liste ile tekrar göster
-            
-        });
-    });
+    li.appendChild(span); // <span> öğesini <li> içine yerleştirin
+
+    listContainer.appendChild(li);
+  }
+
+  remove();
+};
+
+// Bu fonksiyon her öğe için farklı bir simge döndürür
+// Duruma veya öğenin özelliklerine göre farklı simgeler döndürebilirsiniz
+const getSymbolForItem = (index) => {
+    // Özel bir mantıkla her öğe için farklı bir simge döndürün
+    // Burada farklı durumlara göre farklı simgeler döndürebilirsiniz
+    return "\u00d7"; // Örneğin, 'x' işareti
 };
 
 
-listContainer.addEventListener("click", function(e){
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked")
-        
-    } else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        
-    }
-}, false)
+const remove = () => {
+  document.querySelectorAll("span").forEach((span) => {
+    span.addEventListener("click", () => {
+      // console.log("here we go");
+      //? change to 'span'
+      span.parentElement.remove();
+console.log(span.parentElement);
+      let taskText = span.parentElement.textContent;
+      let taskIndex = list.indexOf(taskText);
+      console.log(taskText);//! inputa a yazıyorum, taskText ax oluyor, x li nin içindeki span den, dolayısıyla taskIndex -1 çıkıyor o da bulamayınca sondan siliyor
+console.log(taskIndex);
+      //? delete the task from list
+      list.splice(taskIndex, 1);
+      //? save the updated version of list
+      console.log(list);
+      localStorage.setItem("LIST", JSON.stringify(list));
+      showList(); //? Updated task show again
+    });
+  });
+};
 
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
 
 showList();
---
+++ code/js/23-Checkout Project/checkout.js
@@ -0,0 +1,66 @@
+//* ===================================================
+//*                 Checkout Page Solution
+//*  map filter, dest,spread ===================================================
+//!table da kullanılacak değişkenler
+const kargo = 15.0;
+const vergi = 0.18;
+
+let sepettekiler = [
+  { name: "Vintage Backpack", price: 34.99, adet: 1, img: "./img/photo1.png" },
+  { name: "Levi Shoes", price: 40.99, adet: 1, img: "./img/photo2.png" },
+  { name: "Antique Clock", price: 69.99, adet: 1, img: "./img/photo3.jpg" },
+];
+
+
+
+
+{/* <div class="card mb-3" style="max-width: 540px;">
+
+  <div class="row g-0">
+
+    <div class="col-lg-3 col-md-5">
+      <img src= ""  class=" w-100 rounded-start" alt="..."/>
+    </div>
+
+    <div class="col-md-7 ">
+
+      <div class="card-body">
+      
+        <h5 class="card-title"> </h5>
+        
+             <div class="ürün-price">
+                    <p class="text-warning h2">$
+                      <span class="indirim-price"> </span>
+                      <span class="h5 text-dark text-decoration-line-through"> </span>
+                    </p>
+                  </div>
+
+                  
+                  <div
+                    class="border border-1 border-dark shadow-lg d-flex justify-content-center p-2"
+                  >
+                    <div class="adet-controller">
+                      <button class="btn btn-secondary btn-sm minus">
+                        <i class="fas fa-minus"></i>
+                      </button>
+                      <p class="d-inline mx-4" id="ürün-adet"> </p>
+                      <button class="btn btn-secondary btn-sm plus">
+                        <i class="fas fa-plus"></i>
+                      </button>
+                    </div>
+
+                  </div>
+
+                  <div class="ürün-removal mt-4">
+                    <button class="btn btn-danger btn-sm w-100 remove-ürün">
+                      <i class="fa-solid fa-trash-can me-2"></i>Remove
+                    </button>
+                  </div>
+
+                  <div class="mt-2">
+                    Ürün Toplam: $<span class="ürün-toplam"> </span>
+                  </div>
+      </div>
+    </div>
+  </div>
+</div> */}
