d73ec410683c239071d651db2b11d33557284243
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-16 12:05:54 +0400

    16.10.2023 md

--- code/js/23-Checkout Project/checkout.js
+++ code/js/23-Checkout Project/checkout.js
@@ -11,56 +11,65 @@ let sepettekiler = [
   { name: "Antique Clock", price: 69.99, adet: 1, img: "./img/photo3.jpg" },
 ];
 
+//! EKRANA BASTIR
+sepettekiler.forEach((urun) => {
+
+  //!DESTRUCTURING
+  const { name, price, adet, img } = urun
+
+  document.querySelector("#urun-rowlari").innerHTML += `
+
+  <div class="card mb-3" style="max-width: 540px;">
+
+    <div class="row g-0">
+
+      <div class="col-lg-3 col-md-5">
+        <img src="${img}" class=" w-100 rounded-start" alt="..." />
+      </div>
+
+      <div class="col-md-7 ">
+
+        <div class="card-body">
+
+          <h5 class="card-title">${name} </h5>
+
+          <div class="ürün-price">
+            <p class="text-warning h2">$
+              <span class="indirim-price">${(price*0.7).toFixed(2)} </span>
+              <span class="h5 text-dark text-decoration-line-through">${price} </span>
+            </p>
+          </div>
 
 
+          <div
+            class="border border-1 border-dark shadow-lg d-flex justify-content-center p-2"
+          >
+            <div class="adet-controller">
+              <button class="btn btn-secondary btn-sm minus">
+                <i class="fas fa-minus"></i>
+              </button>
+              <p class="d-inline mx-4" id="ürün-adet">${adet} </p>
+              <button class="btn btn-secondary btn-sm plus">
+                <i class="fas fa-plus"></i>
+              </button>
+            </div>
 
-{/* <div class="card mb-3" style="max-width: 540px;">
+          </div>
 
-  <div class="row g-0">
+          <div class="ürün-removal mt-4">
+            <button class="btn btn-danger btn-sm w-100 remove-ürün">
+              <i class="fa-solid fa-trash-can me-2"></i>Remove
+            </button>
+          </div>
 
-    <div class="col-lg-3 col-md-5">
-      <img src= ""  class=" w-100 rounded-start" alt="..."/>
-    </div>
-
-    <div class="col-md-7 ">
-
-      <div class="card-body">
-      
-        <h5 class="card-title"> </h5>
-        
-             <div class="ürün-price">
-                    <p class="text-warning h2">$
-                      <span class="indirim-price"> </span>
-                      <span class="h5 text-dark text-decoration-line-through"> </span>
-                    </p>
-                  </div>
-
-                  
-                  <div
-                    class="border border-1 border-dark shadow-lg d-flex justify-content-center p-2"
-                  >
-                    <div class="adet-controller">
-                      <button class="btn btn-secondary btn-sm minus">
-                        <i class="fas fa-minus"></i>
-                      </button>
-                      <p class="d-inline mx-4" id="ürün-adet"> </p>
-                      <button class="btn btn-secondary btn-sm plus">
-                        <i class="fas fa-plus"></i>
-                      </button>
-                    </div>
-
-                  </div>
-
-                  <div class="ürün-removal mt-4">
-                    <button class="btn btn-danger btn-sm w-100 remove-ürün">
-                      <i class="fa-solid fa-trash-can me-2"></i>Remove
-                    </button>
-                  </div>
-
-                  <div class="mt-2">
-                    Ürün Toplam: $<span class="ürün-toplam"> </span>
-                  </div>
+          <div class="mt-2">
+            Ürün Toplam: $<span class="ürün-toplam">${(price*0.7).toFixed(2)*adet} </span>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div> */}
+  </div>`
+
+
+})
+
