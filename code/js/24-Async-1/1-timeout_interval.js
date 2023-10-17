57a51a0d26ef885267c3c0059c5ad39af59ceac6
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-17 12:07:41 +0400

    17.10.2023 md

--- code/js/24-Async-1/1-timeout_interval.js
+++ code/js/24-Async-1/1-timeout_interval.js
@@ -18,12 +18,34 @@
 
 //*Senkron Programlama
 //****************************************** */
-
+// // Zorla bekletmek icin boyle gereksiz ve uzun bir kod yapilabilir setTimeout bunun kisa yoludur...
+// const bekle  = (ms) => {
+//     const start = new Date().getTime()
+//     while (new Date().getTime() < start + ms) {}
+// }
+console.log("hello");
+// console.time("timer1") // Kronometre yi baslat //- time ile timeEnd in icinde yazan ayni olmali
+// bekle(3000)
+// console.timeEnd("timer1") // Kronometreyi durdur
 
 
 //*Asenkron (setTimeout) 1 seferlik
 //******************************** */
 
+setTimeout(() => {
+
+    console.log("hello world");
+
+}, 2000)
+
+console.log("world hello");
+
+setTimeout(() => {
+    console.log("Hii World");
+}, 1000);
+
+//!world hello - Hii World - hello world çalışır, 3. nün süresi 1.den azsa, 2-3-1 sırasında çalışır
+//!settimeout müsait zamanda ortaya çıkar, onun sırası geçtiğinde alttaki işlemler bitince çalışır
 
 
 
@@ -31,4 +53,17 @@
 //***************************** */
 
 //?setInterval non-blocking
+
+console.log("Sayac Basliyor");
+
+let sayac = 0;
+
+const interval1 = setInterval(() => {
+    console.log(sayac++);
+
+    if(sayac == 7) clearInterval(interval1)
+
+}, 1000);
+
+console.log("Sayac durduruldu");
 
