ff96a9d5a141d113e2dad8232f0768da8edf98b9
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-22 22:23:27 +0200

    22.01.2023 Sunday update start

--- code/js/22-ToDoApp Project/self/main.js
+++ code/js/22-ToDoApp Project/self/main.js
@@ -1,145 +1,3 @@
-// const inputBox = document.getElementById("input-box");
-// const listContainer = document.getElementById("list-container");
-
-// // let x = []
-// // localStorage.clear();
-
-// function addTask() {
-//     if (inputBox.value === "") {
-//         alert("First you must write something!")
-//     } else {
-//         let li = document.createElement("li")
-//         li.innerHTML = inputBox.value;
-//         listContainer.appendChild(li)
-//         let span = document.createElement("span")
-//         span.innerHTML = "\u00d7"
-//         li.appendChild(span)
-//     }
-//     // x.push(inputBox.value)
-//     inputBox.value = ""
-//     saveData();
-// }
-
-// listContainer.addEventListener("click", function(e){
-//     if (e.target.tagName === "LI") {
-//         e.target.classList.toggle("checked")
-//         saveData()
-//     } else if (e.target.tagName === "SPAN") {
-//         e.target.parentElement.remove();
-//         saveData()
-//     }
-// }, false)
-
-// function  saveData() {
-//     localStorage.setItem("data", listContainer.innerHTML);
-// }
-
-// function showTask() {
-//     listContainer.innerHTML = localStorage.getItem("data")
-// }
-// showTask();
-
-//* -------------------------------------------------------------------------- */
-
-// //? Here is the list for storge
-// const list = JSON.parse(localStorage.getItem("LIST")) || [];
-
-// //? Variables
-// const input = document.getElementById("input-box")
-// const button = document.querySelector(".button")
-// const listContainer = document.getElementById("list-container")
-
-
-// //? When you push the button
-// button.onclick = () => {
-//     if (!input.value) {
-//         alert("Please firstly enter one task")
-//     } else if (list.includes(input.value)) {
-//         alert("You have already this task")
-//     } else {
-//         list.push(input.value)
-//         // console.log(list);
-
-//         //? When you enter the task to the list, update the list 
-//         localStorage.setItem("LIST", JSON.stringify(list))
-
-
-//         // console.log(list);
-        
-//         input.focus();
-    
-//         //? After enter the value return empty input
-//         input.value = ""
-
-//         showList();
-
-//     }
-
-// }
-
-// const showList = () => {
-    
-//     //? Clear the last input in data
-//     listContainer.innerHTML = "";
-    
-//     for (let i = 0; i < list.length; i++) {
-//         const li = document.createElement("li");
-//         li.textContent = list[i];
-        
-//         const span = document.createElement("span");
-//         span.textContent = "\u00d7";
-        
-//         li.appendChild(span);
-//         listContainer.appendChild(li);
-        
-//     }
-    
-//     remove();
-//     // check();
-// }
-
-
-// const remove = () => {
-
-//     document.querySelectorAll("span").forEach((span) => {
-//         span.addEventListener("click", () => {
-            
-//              //? change to 'span'
-//             span.parentElement.remove();
-
-//             let taskText = span.parentElement.textContent;
-//             let taskIndex = list.indexOf(taskText);
-//             console.log(span.parentElement.textContent);
-            
-//             //? delete the task from list
-//            list.splice(taskIndex,1)
-        
-            
-//             //? save the updated version of list
-//             localStorage.setItem("LIST", JSON.stringify(list)); 
-//             showList(); // Görevleri güncel liste ile tekrar göster
-            
-//         });
-//     });
-// };
-
-
-// listContainer.addEventListener("click", function(e){
-//     if (e.target.tagName === "LI") {
-//         e.target.classList.toggle("checked")
-        
-//     } else if (e.target.tagName === "SPAN") {
-//         e.target.parentElement.remove();
-        
-//     }
-// }, false)
-
-
-// showList();
-
-
-/* -------------------------------------------------------------------------- */
-
 
 //? Here is the list for storge
 const list = JSON.parse(localStorage.getItem("LIST")) || [];
@@ -173,33 +31,9 @@ button.onclick = () => {
   }
 };
 
-// const showList = () => {
-//   //? Clear the last input in data
-//   listContainer.innerHTML = "";
-
-//   for (let i = 0; i < list.length; i++) {
-//     const li = document.createElement("li");
-//     li.textContent = list[i];
-
-//     const span = document.createElement("span");
-//     span.textContent = "\u00d7";
-
-//     li.appendChild(span);
-   
-//     listContainer.appendChild(li);
-//   }
-
-//   remove();
-//   // check();
-// };
-
-// const getSymbolForItem = (index) => {
-//     // Özel bir mantıkla her öğe için farklı bir simge döndürün
-//     // Burada farklı durumlara göre farklı simgeler döndürebilirsiniz
-//     return "\u00d7"; // Örneğin, 'x' işareti
-// };
-
 const showList = () => {
+
+  //? Clear the last input in data
   listContainer.innerHTML = "";
 
   for (let i = 0; i < list.length; i++) {
@@ -207,11 +41,11 @@ const showList = () => {
     li.textContent = list[i];
 
     const span = document.createElement("span");
-    const symbol = getSymbolForItem(i); // Her öğe için farklı bir simge alın
+    const symbol = getSymbolForItem(i); 
 
-    span.textContent = symbol; // Sembolü ekleyin
+    span.textContent = symbol; // add symbol
 
-    li.appendChild(span); // <span> öğesini <li> içine yerleştirin
+    li.appendChild(span); // <span> placed into to the <li> 
 
     listContainer.appendChild(li);
   }
@@ -219,12 +53,10 @@ const showList = () => {
   remove();
 };
 
-// Bu fonksiyon her öğe için farklı bir simge döndürür
-// Duruma veya öğenin özelliklerine göre farklı simgeler döndürebilirsiniz
+
 const getSymbolForItem = (index) => {
-    // Özel bir mantıkla her öğe için farklı bir simge döndürün
-    // Burada farklı durumlara göre farklı simgeler döndürebilirsiniz
-    return "\u00d7"; // Örneğin, 'x' işareti
+  // we can do here for every different situation different symbol
+  return "\u00d7"; // exmp. 'x' symbol
 };
 
 
@@ -234,11 +66,11 @@ const remove = () => {
       // console.log("here we go");
       //? change to 'span'
       span.parentElement.remove();
-console.log(span.parentElement);
+      console.log(span.parentElement);
       let taskText = span.parentElement.textContent;
-      let taskIndex = list.indexOf(taskText);
-      console.log(taskText);//! inputa a yazıyorum, taskText ax oluyor, x li nin içindeki span den, dolayısıyla taskIndex -1 çıkıyor o da bulamayınca sondan siliyor
-console.log(taskIndex);
+      let taskIndex = list.indexOf(taskText.slice(0, taskText.length - 1));
+      console.log(taskText);
+      console.log(taskIndex);
       //? delete the task from list
       list.splice(taskIndex, 1);
       //? save the updated version of list
