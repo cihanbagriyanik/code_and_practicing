7f0ea1abb90956a5f82544b332207afcbcdc59f8
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-23 11:28:41 +0200

    23.01.2023 md

--- code/js/27-OOP/2-Object-Constructor.js
+++ code/js/27-OOP/2-Object-Constructor.js
@@ -12,25 +12,22 @@
 //! Book ismi büyük harfle,constructor olduğu biz tarafından anlaşılsın diye. anne karnında bebek var doğarsa değeri olur (üstte constructor anne karnındaki bebek, altta book1 oluşturmak=bebeğin doğması)
 //!arrow function this keyword unu desteklemediği için burada function decleration yazımını tercih ettik
 
-function Book (a, b, c) {
-    
-    (this.author = b),
+function Book(a, b, c) {
+  (this.author = b),
     (this.title = a),
     (this.year = c),
     (this.ozetFunction = function () {
-        return `${this.title} kitabini ${this.year} yilinda ${this.author} yazmistir.`
-    })
-
+      return `${this.title} kitabini ${this.year} yilinda ${this.author} yazmistir.`;
+    });
 }
 
-const book1 = new Book ("karamazov", "dostyevski", 1980)
-const book2 =  new Book ("yaprak dokumu", "resat nuri", 1950)
+const book1 = new Book("karamazov", "dostyevski", 1980);
+const book2 = new Book("yaprak dokumu", "resat nuri", 1950);
 
 console.log(book1);
 console.log(book2);
 
-document.querySelector("h2").textContent = book2.ozetFunction()
-
+document.querySelector("h2").textContent = book2.ozetFunction();
 
 //? new keyword ü  Book Constructor ı parametrelerle çağırır.
 //? Constructor, Book object in bir örneğini kalıbını oluşturur.
@@ -39,11 +36,10 @@ document.querySelector("h2").textContent = book2.ozetFunction()
 //!kalıp ta prototype alanına ulaşmak için alttaki gibi bir syntax kullanılır
 //*Book un prototype alanına yeni bir parametre ekledik ama bellekte yer kaplamaz, ihtiyaç olunca child lar kullanır
 
-
-Book.prototype.tur = "roman"
-Book.prototype.yilHesapla = function() {
-    return 2023 - this.year
-}
+Book.prototype.tur = "roman";
+Book.prototype.yilHesapla = function () {
+  return 2023 - this.year;
+};
 console.log(book2);
 console.log(book2.tur);
 
@@ -51,30 +47,25 @@ console.log(book2.tur);
 
 console.log(book2.yilHesapla());
 
-
-
 //?direk object lerime açıktan bir value eklemek istersem, bellekte yer kaplar, alttaki gibi yazılır
-book1.fiyat = 100
+book1.fiyat = 100;
 console.log(book1);
 console.log(book2);
-
 
 //! OKUL YONETIMINI HATIRLA
 //? INHERITANCE (Miras)
 
-function Dergi(title,author,year,d){
-    Book.call(this,title,author,year)
+function Dergi(title, author, year, d) {
+  Book.call(this, title, author, year);
 
-    this.month = d 
+  this.month = d;
 }
 
 //*Book object inden (kalıbından) türetilen Dergi kalıbına, Book un prototype alanındaki bilgiler (ES5 te) direk gelmez,gelmesini istiyorsak alttaki kodu (daha child oluşturmadan önce)yazmalıyız
-Dergi.prototype = Object.create(Book.prototype)
+Dergi.prototype = Object.create(Book.prototype);
 
-
-const dergi1 = new Dergi("kafa", "ibrahim", 1980, "eylul")
-const dergi2 = new Dergi("nokta", "hasan can", 1960, "kasim")
-
+const dergi1 = new Dergi("kafa", "ibrahim", 1980, "eylul");
+const dergi2 = new Dergi("nokta", "hasan can", 1960, "kasim");
 
 console.log(dergi1);
 console.log(dergi1.ozetFunction());
