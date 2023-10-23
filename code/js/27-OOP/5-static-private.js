37440bcdb962da9f2b45761558ff73c2a72e29a6
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-23 12:25:52 +0200

    23.01.2023 md

--- code/js/27-OOP/5-static-private.js
+++ code/js/27-OOP/5-static-private.js
@@ -22,3 +22,62 @@
 
 //! Private metotlara ise class disarisindan da erisilemez.
 //! Private metotlara ancak class icerisindeki diger metotlar ile erisilebilir.
+
+class Book {
+    //!private değişken tanımlanması
+    #id;
+    #name;
+    //!static değişken tanımlanması
+    static counter = 0;
+  
+    constructor(title, author, year) {
+      this.title = title;
+      this.author = author;
+      this.year = year;
+      this.#id = 44;
+      this.#name = "osman";
+  
+      this.getirTitle = function () {
+        return this.title;
+      };
+      Book.counter++;
+      Book.id++;
+    }
+  
+    //!alttaki kod, atanın (Book) child lara id değişkenini görme izni
+    getId() {
+      return this.#id;
+    }
+    getName() {
+      return this.#name;
+    }
+  
+    //!alttaki kod, Book kalıbının, child lara update izni vermesi
+    setId(id){
+  
+      this.#id=id
+    }
+  
+    
+  }
+  const book1=new Book("simyacı", "poelho coelgo", 1988)
+  console.log(book1);
+  console.log(Book.counter);
+  console.log(++Book.counter);
+  console.log(book1.id);//private değişken bu şekilde gelmez
+  
+  
+  //!private değişkenlerine, Book kalıbında izin vererek alttaki gibi çağırdık
+  console.log(book1.getId());
+  console.log(book1.getName());
+  
+  // book1.#id=111
+  
+  book1.setId(111)
+  
+  console.log(book1.getId());
+  
+  
+  //!setter sız private değişkenleri değiştiremeyiz
+  // book1.#name="ayse"
+  console.log(book1.getName());
