e50dc7112c15b49a2a69273208cc82de961a83c8
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-25 11:10:28 +0200

    25.10.2023 props end

--- code/react/02-props/src/App.js
+++ code/react/02-props/src/App.js
@@ -1,4 +1,7 @@
 
+import People from "./People"
+
+
 
 
 const App = () => {
@@ -6,7 +9,18 @@ const App = () => {
   return (
     <div>
 
-      App
+      <People
+        name="Osman Kara"
+        img="https://cdn.pixabay.com/photo/2017/08/01/01/33/beanie-2562646__480.jpg"
+        tel="555 55 55"
+
+      />
+      <People
+        name="Ipek Bilir"
+        img="https://cdn.pixabay.com/photo/2020/10/19/09/44/woman-5667299_1280.jpg"
+        tel="555 44 44"
+      />
+
 
     </div>
   )
