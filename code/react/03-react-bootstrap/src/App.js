5bb7637a0d5f69e82b09e0ad97d7eba62e4d1a0d
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-10-25 12:34:00 +0200

    25.10.2023 react-bootstrap md

--- code/react/03-react-bootstrap/src/App.js
+++ code/react/03-react-bootstrap/src/App.js
@@ -1,12 +1,18 @@
 
 
+import MyNavbar from "./components/MyNavbar"
+
+//! Bootstrap in yolunu da import ediyoruz
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 const App = () => {
 
   return (
     <div>
 
-      App
+      <MyNavbar />
 
     </div>
   )
