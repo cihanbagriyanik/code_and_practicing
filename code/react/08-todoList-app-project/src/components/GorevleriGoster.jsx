a3223bf8581da72859685e93026cebd577f9b3a4
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-08 12:02:54 +0100

    08.11.2023 TodoApp  md

--- code/react/08-todoList-app-project/src/components/GorevleriGoster.jsx
+++ code/react/08-todoList-app-project/src/components/GorevleriGoster.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { FaTimesCircle } from "react-icons/fa";
 
 
-const GorevleriGoster = () => {
+const GorevleriGoster = ({ gorevler, setGorevler }) => {
+  // console.log(gorevler);
+
+  const deleteGorev = (sil) => {
+    setGorevler(gorevler.filter((i) => i.id !== sil))
+  }
+
   return (
-    <div>GorevleriGoster</div>
+    <div>
+
+      {gorevler.map((x) => {
+        return (
+          <div
+            key={x.id}
+            className={x.isDone ? "done" : "gorev"}
+            onDoubleClick={() => setGorevler(gorevler.map((a) => a.id === x.id ? { ...a, isDone: !a.isDone } : a))}
+          >
+
+            <h3> {x.text} <FaTimesCircle onClick={() => deleteGorev(x.id)} /> </h3>
+            <h6> {x.day}  </h6>
+
+          </div>
+        )
+      })}
+
+
+
+    </div>
   )
 }
 
--- code/react/08-todoList-app-project/src/pages/Home.jsx
+++ code/react/08-todoList-app-project/src/pages/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import GorevEkle from '../components/GorevEkle'
+import GorevleriGoster from '../components/GorevleriGoster'
+
+import Data from "../helper/Data"
+import { useState } from 'react'
 
 const Home = () => {
+
+  const [gorevler, setGorevler] = useState(Data)
+  // console.log(gorevler);
+
+
   return (
-    <div>Home</div>
+    <div>
+
+      <GorevEkle gorevler={gorevler} setGorevler={setGorevler} />
+
+      <GorevleriGoster gorevler={gorevler} setGorevler={setGorevler} />
+
+    </div>
   )
 }
 
