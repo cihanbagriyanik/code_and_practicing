f15b115a45e370278bed6e1965cdc34888890634
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-08 01:53:49 +0100

    08.11.2023 TodoApp Start

---
+++ code/react/08-todoList-app-project/src/App.js
@@ -0,0 +1,20 @@
+import Header from "./components/Header"
+import TodoList from "./components/TodoList"
+
+import "./App.css"
+
+const App = () => {
+
+  return (
+    <div>
+
+      <Header />
+
+      <TodoList />
+
+    </div>
+  )
+}
+
+
+export default App
