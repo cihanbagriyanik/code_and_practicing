3d9a267efd06b66511cf7dd762589f3e0fc9d6c5
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-08 09:22:19 +0100

    08.11.2023 TodoApp Start update

--- code/react/08-todoList-app-project/src/components/GorevEkle.jsx
+++ code/react/08-todoList-app-project/src/components/GorevEkle.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const GorevEkle = () => {
   return (
     <div>GorevEkle</div>
--- code/react/08-todoList-app-project/src/components/GorevleriGoster.jsx
+++ code/react/08-todoList-app-project/src/components/GorevleriGoster.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const GorevleriGoster = () => {
   return (
     <div>GorevleriGoster</div>
