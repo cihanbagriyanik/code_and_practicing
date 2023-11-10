3da61fdaabb91471535b33bd45b395a964a8359c
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-10 12:40:19 +0100

    appointment app class end

--- code/react/09-appointment-app-project/src/components/HastaEkle.jsx
+++ code/react/09-appointment-app-project/src/components/HastaEkle.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const HastaEkle = () => {
+const HastaEkle = ({hastalar,setHastalar,doktorlar}) => {
+const[patientName,setName]=useState("")
+const[tarih,setTarih]=useState("")
+ console.log(doktorlar);//doktorlar dizisi tek elemanlı geldi
+const handleSubmiT=(e)=>{
+  e.preventDefault(); //direk submit olayı yapmadan önce alttaki kodlara bak
+  // let yeniHasta = { id: 7, text: patientName, day: tarih, isDone: false };
+  setHastalar([
+    ...hastalar,
+    { id: hastalar.length+1, text: patientName, day: tarih, isDone: false, myDoctor: doktorlar[0].doctorName },
+  ]);
+
+  // submit sonrası inputlardan value temizlemek için, hem alttaki işlemler yapılır, hemde inputlarda value={isim} yazarak browser da boşluksa boşluk isimse isim gözükmesi sağlanır
+
+  setName("");
+  setTarih("");
+}
+
+
+
+
+
+console.log(hastalar);
+
   return (
-    <div>HastaEkle</div>
-  )
+    <div>
+      <form onSubmit={handleSubmiT}>
+        <div className="form-control">
+          <label htmlFor="text"> Hasta Bilgileri</label>
+          <input
+            type="text"
+            id="text"
+            onChange={(e) => setName(e.target.value)}
+            value={patientName}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="day"> Day & Time</label>
+          <input
+            type="datetime-local"
+            id="day"
+            onChange={(e) => setTarih(e.target.value)}
+            value={tarih}
+          />
+        </div>
+
+        <div>
+          <button className="dok" type="submit">
+            <span style={{ color: "#6a0707" }}>{doktorlar[0].doctorName}</span> için kayıt
+            oluştur
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default HastaEkle
--- code/react/09-appointment-app-project/src/pages/Home.jsx
+++ code/react/09-appointment-app-project/src/pages/Home.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import HastaEkle from "../components/HastaEkle";
+import HastaListe from "../components/HastaListe";
+import { doctorData, hastaData } from "../helper/Data";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [hastalar, setHastalar] = useState(hastaData);
+  const [doktorlar, setDoktorlar] = useState(doctorData);
+const [tikla,setTikla]=useState(true)
 
-export default Home
+
+const doctorClick=(abc)=>{
+//tıkla yı değiştir
+setTikla(!tikla)
+
+//doktorlar dizisini tıklanan doktorla güncelle
+setDoktorlar(tikla ? doktorlar.filter((i)=>i.id===abc):doctorData)
+
+}
+  return (
+    <div style={{ display: tikla ? "block" : "flex" }}>
+      <div>
+        <header className="header">
+          <h1>HOSPITAL</h1>
+
+          <div className="dr">
+            {doktorlar.map((dr) => (
+              <div key={dr.id}>
+                <img
+                  src={dr.doctorImg}
+                  alt=""
+                  width="180px"
+                  height="150px"
+                  className="btn"
+                  style={{ backgroundColor: tikla ? "aqua" : "lightgreen" }}
+                  onClick={() => doctorClick(dr.id)}
+                />
+
+                <h4
+                  style={{
+                    backgroundColor: tikla ? "aqua" : "lightgreen",
+                    borderLeft: `10px solid ${tikla ? "blue" : "green"}`,
+                  }}
+                >
+                  {dr.doctorName}
+                </h4>
+              </div>
+            ))}
+          </div>
+        </header>
+
+        {!tikla && (
+          <HastaEkle
+            hastalar={hastalar}
+            setHastalar={setHastalar}
+            doktorlar={doktorlar}
+          />
+        )}
+      </div>
+
+      <HastaListe
+        hastalar={hastalar}
+        setHastalar={setHastalar}
+        doktorlar={doktorlar}
+      />
+    </div>
+  );
+};
+
+export default Home;
