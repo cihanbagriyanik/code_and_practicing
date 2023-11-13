7715c2eb53fc5d0152da4b2f8315e9c7b010e970
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-13 09:54:46 +0100

    13.11.2023 lifeCycle end - useEfect start

--- code/react/10-lifeCycle/src/App.js
+++ code/react/10-lifeCycle/src/App.js
@@ -1,12 +1,26 @@
 
-
+import { useState } from "react"
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import LifeCycleMethods from "./components/LifeCycleMethods"
 
 const App = () => {
 
-  return (
-    <div>
+  const [show, setShow] = useState(true)
 
-      App
+  return (
+    <div className="container text-center mt-4">
+
+      <button
+        className="btn btn-danger"
+        onClick={() => setShow(!show)}
+      >
+        SHOW
+      </button>
+
+
+      {show && <LifeCycleMethods />}
+
 
     </div>
   )
