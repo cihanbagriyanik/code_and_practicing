c6fb349dba39c8e95e91f69d5e02d881bba09f62
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-14 11:25:47 +0100

    14.11.2023 add bilgi update

--- code/react/11-useEffect-database-axios/src/components/BilgiList.jsx
+++ code/react/11-useEffect-database-axios/src/components/BilgiList.jsx
@@ -9,7 +9,7 @@ const BilgiList = ({ tutorial }) => {
 
 
   return (
-    <div>
+    <div className='container mt-4'>
       <table className="table table-striped">
         <thead>
           <tr>
@@ -20,31 +20,67 @@ const BilgiList = ({ tutorial }) => {
           </tr>
         </thead>
         <tbody>
+          {tutorial.map(({ id, title, description }) => {
+            return (
+              <tr >
+                <th> {id} </th>
+                <td> {title} </td>
+                <td> {description} </td>
+                <td className="text-center ">
+                  <AiFillDelete
+                    type="button"
+                    size={22}
+                    className="text-danger cursor-pointer"
+                  />
 
-          <tr >
-            <th></th>
-            <td></td>
-            <td> </td>
-            <td className="text-center ">
-              <AiFillDelete
-                type="button"
-                size={22}
-                className="text-danger cursor-pointer"
-              />
-
-              <FaEdit
-                data-bs-toggle="modal"
-                data-bs-target="#editModal"
-                size={20}
-                type="button"
-                className="me-2 text-warning cursor-pointer"
-              />
-            </td>
-          </tr>
-
-
+                  <FaEdit
+                    data-bs-toggle="modal"
+                    data-bs-target="#editModal"
+                    size={20}
+                    type="button"
+                    className="me-2 text-warning cursor-pointer"
+                  />
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* -------------------------------------------------------------------------- */}
+      {/* edit modul */}
+
+      {/* <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div> */}
+
+      {/* -------------------------------------------------------------------------- */}
     </div>
 
   )
--- code/react/11-useEffect-database-axios/src/pages/Home.jsx
+++ code/react/11-useEffect-database-axios/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import BilgiList from "../components/BilgiList";
+import AddBilgi from "../components/AddBilgi";
 
 const Home = () => {
   const [tutorial, setTutorial] = useState([]);
@@ -9,7 +10,6 @@ const Home = () => {
   const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
 
   //! GET işlemi
-
   const getTutorialS = async () => {
     const veri = await axios.get(BASE_URL);
 
@@ -21,11 +21,24 @@ const Home = () => {
     getTutorialS();
   }, []);
 
+  console.log(tutorial);
+
+
+  //!POST İşlemi
+  const postBilgi = async (veri) => {
+    await axios.post(BASE_URL, veri);
+  }
+
+
+
   return <>
+
+    <AddBilgi postBilgi={postBilgi} />
 
     <BilgiList tutorial={tutorial} />
 
-  </>;
+  </>
+
 };
 
 export default Home;
