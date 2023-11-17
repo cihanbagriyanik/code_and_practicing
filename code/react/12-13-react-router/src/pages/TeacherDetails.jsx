0821b770a56b90b6ef380d828f86b984a72ae85c
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-17 11:15:32 +0100

    17.11.2023 roueter 2. days spin

--- code/react/12-13-react-router/src/pages/TeacherDetails.jsx
+++ code/react/12-13-react-router/src/pages/TeacherDetails.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 
+import spin from "../img/Spin-1s-200px.gif";
+import NotFound from "./NotFound";
+
 const TeacherDetails = () => {
   //!!!!2.YOL useLocation.//  navigate ile gonderilen state'i yakalamak icin useLocation Hook'u kullanilabilir.
   //! Bu durumda veri, state ile geldigi icin yeniden fetch yapilmasina gerek kalmaz
@@ -11,14 +14,30 @@ const TeacherDetails = () => {
   /* -------------------------------------------------------------------------- */
   //? 1.YOL fetch ile buraya gonderilen id li veriyi tekrar cekmek
   // const { id } = useParams();
-
   // const [kisi, setKisi] = useState({});
+
+  // const [error, setError] = useState(false);
+  // const [loading, setLoad] = useState(true);
 
   // useEffect(() => {
   //   fetch(`https://reqres.in/api/users/${id}`)
   //     .then((res) => res.json())
-  //     .then((data) => setKisi(data.data));
+  //     .then((data) => setKisi(data.data))
+  //     .catch((err) => {
+  //       setError(true);
+  //     })
+  //     .finally(() => setLoad(false));
   // }, [id]);
+
+  // if (error) {
+  //   return <NotFound />;
+  // } else if (loading) {
+  //   return (
+  //     <div className="container text-center mt-4">
+  //       <img src={spin} alt="" />
+  //     </div>
+  //   );
+  // }
 
   // return (
   //   <div className="container text-center mt-4">
@@ -26,7 +45,7 @@ const TeacherDetails = () => {
   //     <h2 className="text-danger">
   //       {kisi.first_name} {kisi.last_name}{" "}
   //     </h2>
-  //     <h3 className="text-primary"> Benim emil im :  {kisi.email} </h3>
+  //     <h3 className="text-primary"> Benim emil im : {kisi.email} </h3>
   //   </div>
   // );
   //?????????????????????
