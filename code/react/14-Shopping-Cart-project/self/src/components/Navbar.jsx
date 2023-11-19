4e0f28e2860632593f6b5961fe16b9ee5e3c5f26
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-11-19 18:29:54 +0100

    17.11.2023 shopping-cart start

--- code/react/14-Shopping-Cart-project/self/src/components/Navbar.jsx
+++ code/react/14-Shopping-Cart-project/self/src/components/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Navbar = () => {
   return (
-   <div></div>
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Shopping Cart</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">New product</Nav.Link>
+              <Nav.Link href="#link">Product List</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
