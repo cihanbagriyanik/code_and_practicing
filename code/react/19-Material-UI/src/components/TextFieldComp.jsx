d19f5addeed9cca8f8479957e80bb51bbb202953
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-12-05 12:36:18 +0100

    05.12.2023 materialui endClass

--- code/react/19-Material-UI/src/components/TextFieldComp.jsx
+++ code/react/19-Material-UI/src/components/TextFieldComp.jsx
@@ -1,5 +1,7 @@
 import {
+  Avatar,
   Box,
+  Button,
   Container,
   FormControl,
   IconButton,
@@ -113,6 +115,15 @@ const TextFieldComp = () => {
               label="Password"
             />
           </FormControl>
+
+          <Button fullWidth variant="contained" color="error" sx={{ mt: 4 }}>
+            SUBMIT
+            <Avatar
+              sx={{ margin: 4 }}
+              alt="Remy Sharp"
+              src="https://media.istockphoto.com/id/1278564529/tr/foto%C4%9Fraf/mavi-renk-arka-plan-izole-ye%C5%9Fil-g%C3%B6mlek-giyen-esmer-k%C4%B1sa-sa%C3%A7l%C4%B1-sevimli-g%C3%BCl%C3%BCmseyen-g%C3%BCzel-k%C4%B1z.jpg?s=612x612&w=0&k=20&c=qMtjAb1sXxeqOgUHYjWskspY58UyahQsyeu7ZPVgtUQ="
+            />
+          </Button>
         </Box>
       </Typography>
     </Container>
