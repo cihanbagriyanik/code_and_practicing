7f966fbedd2b7e0767b5ae9001767d181eec0e62
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-12-08 12:15:23 +0100

    08.12.2023 vite and pnpm END

--- code/react/22-vite/vite-project/src/App.jsx
+++ code/react/22-vite/vite-project/src/App.jsx
@@ -1,10 +1,15 @@
 // import "./App.css";
+import MyCard from "./components/MyCard";
 import Navbar from "./components/Navbar";
+
+import AcUnitIcon from "@mui/icons-material/AcUnit";
 
 function App() {
   return (
     <>
       <Navbar />
+      <MyCard />
+      <AcUnitIcon fontSize="large" />
     </>
   );
 }
