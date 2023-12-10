2611caf10c587a124d597fa05dc68453f007d8d0
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2023-12-10 18:12:56 +0100

    10.12.2023 stockApp and formikYuo start

---
+++ code/react/23-30-Stock-App/inclass-formik-yup/src/features/authSlice.jsx
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+
+  initialState: {
+    currentUser: null,
+    loading: false,
+    error: false,
+    isAdmin: false,
+    token: null,
+  },
+  reducers: {
+    fetchStart: state => {
+      state.loading = true;
+      state.error = false;
+    },
+    fetchFail: state => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const {
+  fetchStart,
+  fetchFail,
+} = authSlice.actions;
+export default authSlice.reducer;
