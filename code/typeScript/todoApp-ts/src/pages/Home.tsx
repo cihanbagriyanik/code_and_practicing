4ea9e7a498ed92023bc256e5078f381a31597e7c
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-01-04 10:20:57 +0100

    04.01.2024 typeScript todoApp App.tsx end Home.tsx Start

--- code/typeScript/todoApp-ts/src/pages/Home.tsx
+++ code/typeScript/todoApp-ts/src/pages/Home.tsx
@@ -1,7 +1,19 @@
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
+
+interface ITodoType {
+  todo: string;
+  isDone: boolean;
+  id: string | number;
+  task?: string; //!optional
+}
 
 const Home = () => {
+  // const [todos, setTodos] = useState([] as ITodoType[]);
+  // const [todos, setTodos] = useState<Array<ITodoType>>([]);
+  const [todos, setTodos] = useState<ITodoType[]>([]);
+
   return (
     <Container>
       <Typography color="error" align="center" variant="h2" component="h1">
