47cb176a22e946b345fc16596618d6f877b25796
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-01-08 12:37:21 +0100

    08.01.2023 cypress.js 3.allClass END

---
+++ code/cypress.js/recipe-app-cypress/cypress/e2e/details.cy.js
@@ -0,0 +1,19 @@
+describe('recipe project', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+        cy.viewport(1200, 1200);
+    })
+    it('login', () => {
+        cy.login()
+        cy.get("[data-test='homeSearch']").type("r")
+        cy.get("[data-test='homeSearchBtn']").click({ force: true });
+        cy.get("[data-test='cardHeader']").should("be.visible")
+        cy.get("[data-test='cardImage']").should("be.visible")
+        cy.get("[data-test='cardBtn']").should("be.visible").first().click({ force: true });
+        cy.url().should("include", "/details");
+        cy.get("[data-test='contentHeader']").should("be.visible")
+        cy.get("[data-test='contentParag']").should("be.visible")
+        cy.get("[data-test='contentCal']").should("be.visible")
+        cy.get("[data-test='contentImage']").should("be.visible")
+    })
+})
